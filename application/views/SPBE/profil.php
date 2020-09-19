e178804aed5c92f9dc388a25dc714e7b2ce0f2ea
imamaja1 <imam.aja0022@gmail.com>
2020-09-20 00:01:38 +0800

    p sama nama

--- application/views/SPBE/profil.php
+++ application/views/SPBE/profil.php
@@ -37,8 +37,6 @@
         <div class="main-header">
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="blue">
-
-                <h1 style="color: aliceblue; text-align:center">P</h1>
                 <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
                         <i class="icon-menu"></i>
